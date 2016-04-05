8dacc3c4000867c0458cd091971c8b5e17c32145
neos1993 <vrozzuvaylo@gmail.com>
2016-04-05 10:22:58 +0300

    commit new somethings

---
+++ ImportOrderFromApplication/ImportOrderFromApplication/ImportOrderFromApplication/CrmManager.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+
+namespace ImportOrderFromApplication
+{
+    public class CrmManager
+    {
+        private const string processServiceUri = "https://parnikrb-debug.bpmonline.com/1/rest/SxRunProcessAddingOrderFromSiteService/";
+        private const string authServiceUri = "https://parnikrb-debug.bpmonline.com/ServiceModel/AuthService.svc/Login";
+
+        private static CookieContainer AuthCookie = new CookieContainer();
+
+        public static bool IsAuthentificated
+        {
+            get { return (AuthCookie != null); }
+        }
+
+        public static bool TryLogin(string userName, string userPassword)
+        {
+            var authRequest = HttpWebRequest.Create(authServiceUri) as HttpWebRequest;
+            authRequest.Method = "POST";
+            authRequest.ContentType = "application/json";
+            authRequest.CookieContainer = AuthCookie;
+
+            using (var requesrStream = authRequest.GetRequestStream())
+            using (var writer = new StreamWriter(requesrStream))
+                writer.Write(@"{
+                    ""UserName"":""" + userName + @""",
+                    ""UserPassword"":""" + userPassword + @"""
+                }");
+
+            using (var response = (HttpWebResponse)authRequest.GetResponse())
+                return (AuthCookie.Count > 0);
+        }
+
+        public static string Run(string data, string method)
+        {
+            if (!IsAuthentificated) return "";
+
+            string requestString = string.Format(processServiceUri + method);
+            HttpWebRequest request = HttpWebRequest.Create(requestString) as HttpWebRequest;
+            request.Method = "POST";
+            request.ContentType = "application/json";
+            request.ContentLength = data.Length;
+            request.CookieContainer = AuthCookie;
+
+            using (var requesrStream = request.GetRequestStream())
+            using (var writer = new StreamWriter(requesrStream))
+                writer.Write(data);
+
+            var result = string.Empty;
+
+            using (var response = request.GetResponse())
+            using (var reader = new System.IO.StreamReader(response.GetResponseStream()))
+            {
+                result = reader.ReadToEnd();
+                Console.WriteLine(result);
+            }
+            return result;
+        }
+    }
+}
---
+++ ImportOrderFromApplication/ImportOrderFromApplication/ImportOrderFromApplication/DBConnect.cs
@@ -0,0 +1,151 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+
+namespace ImportOrderFromApplication
+{
+    class DBConnect
+    {
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string username;
+        private string password;
+
+        //Constructor
+        public DBConnect()
+        {
+            Initialize();
+        }
+
+        //Initialize values
+        private void Initialize()
+        {
+            server = ConfigurationManager.AppSettings["server"].ToString();
+            //port = ConfigurationManager.AppSettings["port"].ToString();
+            username = ConfigurationManager.AppSettings["username"].ToString();
+            password = ConfigurationManager.AppSettings["password"].ToString();
+            database = ConfigurationManager.AppSettings["database"].ToString();
+            password = "password";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + username + ";" + "PASSWORD=" + password + ";";
+
+            connection = new MySqlConnection(connectionString);
+        }
+
+        //open connection to database
+        private bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                //When handling errors, you can your application's response based 
+                //on the error number.
+                //The two most common error numbers when connecting are as follows:
+                //0: Cannot connect to server.
+                //1045: Invalid user name and/or password.
+                switch (ex.Number)
+                {
+                    case 0:
+                        Console.WriteLine("Cannot connect to server.  Contact administrator");
+                        break;
+
+                    case 1045:
+                        Console.WriteLine("Invalid username/password, please try again");
+                        break;
+                }
+                return false;
+            }
+        }
+
+        //Close connection
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        //Select statement
+        public List<string>[] Select()
+        {
+            string query = "SELECT * FROM tableinfo";
+
+            //Create a list to store the result
+            List<string>[] list = new List<string>[3];
+            list[0] = new List<string>();
+            list[1] = new List<string>();
+            list[2] = new List<string>();
+
+            //Open connection
+            if (this.OpenConnection() == true)
+            {
+                //Create Command
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                //Create a data reader and Execute the command
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                //Read the data and store them in the list
+                while (dataReader.Read())
+                {
+                    list[0].Add(dataReader["id"] + "");
+                    list[1].Add(dataReader["name"] + "");
+                    list[2].Add(dataReader["age"] + "");
+                }
+
+                //close Data Reader
+                dataReader.Close();
+
+                //close Connection
+                this.CloseConnection();
+
+                //return list to be displayed
+                return list;
+            }
+            else
+            {
+                return list;
+            }
+        }
+
+        //Count statement
+        public int Count()
+        {
+            string query = "SELECT Count(*) FROM tableinfo";
+            int Count = -1;
+
+            //Open Connection
+            if (this.OpenConnection() == true)
+            {
+                //Create Mysql Command
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                //ExecuteScalar will return one value
+                Count = int.Parse(cmd.ExecuteScalar() + "");
+
+                //close Connection
+                this.CloseConnection();
+
+                return Count;
+            }
+            else
+            {
+                return Count;
+            }
+        }
+
+    }
+}
